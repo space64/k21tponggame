58f0d56b01f7989ccd23da59f693388d2998c351
Huyen <xspace64@gmail.com>
2016-11-13 08:08:50 +0700

    Khai bao cung 1 JPanel, de dang sua de chay 2 class demo khac nhau

--- k21t/PongGame/src/dialogAndMouseDemo/MainGui.java
+++ k21t/PongGame/src/dialogAndMouseDemo/MainGui.java
@@ -1,14 +1,16 @@
 package dialogAndMouseDemo;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class MainGui extends JFrame {
-	CanvasPanel pnl;
+	JPanel pnl;
 	
 	public MainGui() {
 		setTitle("Demo");
 		pnl = new CanvasPanel();
+		//pnl = new CanvasPanelBallIntersect();
 		setContentPane(pnl);
 		pack();
 		setLocationRelativeTo(null);
@@ -18,7 +20,6 @@ public class MainGui extends JFrame {
 		MainGui f = new MainGui();
 		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		f.setVisible(true);
-
 	}
 
 }
